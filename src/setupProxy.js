c8fa0d5ac28a1a2edf2190396e97ec75a49d4e16
Sajal Aggarwal <sajal72003@gmail.com>
2024-05-26 11:10:15 +0530

    backend port commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "https://chatgpt-backend-1ss4.onrender.com",
       changeOrigin: true,
     })
   );
